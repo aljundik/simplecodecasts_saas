3dd183114844a367a4940d93029ed08bff0222b3
aljundik <kaledj007@gmail.com>
2015-07-16 19:45:32 +0000

    minor changes and improve performance

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,6 @@
 class Contact < ActiveRecord::Base
+    validates :name, presence: true
+    validates :email, presence: true # it makes sure that there is somthign was written in the field and its not embty
     
     
 end
