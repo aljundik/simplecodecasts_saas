842bd72a52655e0dc956c100c326856d44968df6
aljundik <kaledj007@gmail.com>
2015-07-16 20:12:56 +0000

    added mailer view

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,8 +7,14 @@ class ContactsController < ApplicationController
     @contact = Contact.new(contact_params)
     
     if @contact.save
+      name = params[:contact][:name]# get the quires from the form and and assign it to name parameter// this synatx called hashes , where contacts are the hashe's name and name is the key value
+      email = params[:contact][:email]
+      body = params[:contact][:comments]
+    
+      ContactMailer.contact_email(name, email, body).deliver# pass the parameters to ContactMailer class contact_email message in particular
       flash[:success] = 'Message sent.'
       redirect_to new_contact_path
+      
     else
       flash[:danger] = 'Error occured, message has not been sent.'
       redirect_to new_contact_path
