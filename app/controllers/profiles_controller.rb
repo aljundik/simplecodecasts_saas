8b0919d3c9cf26197ea78381febf8e298bc21f59
aljundik <kaledj007@gmail.com>
2015-07-25 12:57:11 +0000

    update the home page

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,8 +2,8 @@ class ProfilesController < ApplicationController
      def new
     # form where a user can fill out their own profile.
     @user = User.find( params[:user_id] ) # find the user id that actully logged in 
-
-    @profile = @user.build_profile # to build a profile for that particular user ,,build_profile is a method available for us by rails , since we have established association in the data base and the routes file as well
+    @profile = Profile.new
+   # @profile = @user.build_profile # to build a profile for that particular user ,,build_profile is a method available for us by rails , since we have established association in the data base and the routes file as well
   end
   def create 
     @user = User.find( params[:user_id] )
