47423cde4973a1a3697d12249e7986313212bc0f
aljundik <kaledj007@gmail.com>
2015-07-25 11:37:51 +0000

    add create new profile action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,3 +1,9 @@
 class ProfilesController < ApplicationController
+     def new
+    # form where a user can fill out their own profile.
+    @user = User.find( params[:user_id] ) # find the user id that actully logged in 
+    @variable = params[:hello]
+    @profile = @user.build_profile # to build a profile for that particular user ,,build_profile is a method available for us by rails , since we have established association in the data base and the routes file as well
+  end
   
 end
