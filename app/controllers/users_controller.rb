2e1aa57f6ce4ba17c337545dacd509f7ae521d4e
aljundik <kaledj007@gmail.com>
2015-07-27 11:00:00 +0000

    added new community page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
   
   def index
+    @users = User.includes(:profile) # save all the users into the instant variable
   end
   def show
     @user = User.find( params[:id] )
